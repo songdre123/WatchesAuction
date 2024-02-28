1f267e638f7e0832797bc08226ae8570c259a3de
Zavier <zavier.yan.2022@scis.smu.edu.sg>
2024-02-28 23:04:34 +0800

    Installed Frontend Framework

---
+++ frontend/watchauction/src/components/auction_card.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="card">
+
+      <div class="card-header text-center">
+        <h5>{{ date }}</h5>
+      </div>
+  
+      <div class="card-body" :style="{ backgroundImage: 'url(' + imageUrl + ')' }">
+ 
+        <div class="card-title text-center text-white">
+          <h4>{{ watchName }}</h4>
+          <p>Reference Number: {{ referenceNumber }}</p>
+          <p>Opening Bid: {{ openingBid }}</p>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        imageUrl: '',
+        date: '',
+        watchNam: '',
+        referenceNumber: '',
+        openingBid: 0,
+      };
+    },
+    created() {
+      this.fetchImage();
+      this.fetchListingDetails();
+    },
+    methods: {
+      fetchImage() {
+        //
+      },
+      fetchListingDetails() {
+        //
+      },
+
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .card {
+    margin: 20px;
+  }
+  
+  .card-body {
+    height: 200px;
+    background-size: cover;
+    background-position: center;
+  }
+  
+  .card-title {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 15px;
+    background-color: rgba(0, 0, 0, 0.7);
+  }
+  </style>
+  
