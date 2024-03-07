20d591dbf78a9e7617e65d0c2bf9cb54c359d1d1
wang-kaijie <99939212+wang-kaijie@users.noreply.github.com>
2024-03-07 13:51:49 +0800

    add in notification sql

---
+++ notification.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS `Notification` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `Notification`;
+DROP TABLE IF EXISTS `Notification`;
+
+CREATE TABLE Notification(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    recipient_id INT NOT NULL,
+    auction_id INT,
+    notification_type VARCHAR(50) NOT NULL COMMENT '(outbid, winandpayremind, paysucess,rollbackandpayremind, schedulesuccess)',
+    time_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_unread INT DEFAULT 1
+);
