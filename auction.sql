4a35e2e7e136333f913c90529d5933460f05c536
songdre123 <129580329+songdre123@users.noreply.github.com>
2024-03-07 10:24:26 +0800

    Create auction.sql

---
+++ auction.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS `Auction` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `Auction`;
+DROP TABLE IF EXISTS `Auction`;
+CREATE TABLE IF NOT EXISTS `Auction` (
+    auction_id INT AUTO_INCREMENT PRIMARY KEY,
+    auction_item VARCHAR(255) NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NULL,
+    start_price FLOAT NOT NULL,
+    current_price FLOAT NOT NULL,
+    auction_winner_id INT,
+    auction_status INT DEFAULT 1,
+    watch_condition VARCHAR(255) NOT NULL,
+    watch_brand VARCHAR(255) NOT NULL,
+    watch_box_present BOOLEAN NOT NULL,
+    watch_papers_present BOOLEAN NOT NULL,
+    watch_image1 VARCHAR(255) NOT NULL,
+    watch_image2 VARCHAR(255) NOT NULL,
+    watch_image3 VARCHAR(255) NOT NULL
+);
