993b0ef2492bd82e68030eb16b99a5f0f3259ad4
e-tayfw <ethantay321@gmail.com>
2024-03-17 12:37:27 +0800

    Update auction.sql
    Auction table updated, included watch_ref, stripe product id

--- sql/auction.sql
+++ sql/auction.sql
@@ -10,11 +10,13 @@ CREATE TABLE IF NOT EXISTS `Auction` (
     current_price FLOAT NOT NULL,
     auction_winner_id INT,
     auction_status INT DEFAULT 1,
+    watch_ref VARCHAR(255) NOT NULL,
     watch_condition VARCHAR(255) NOT NULL,
     watch_brand VARCHAR(255) NOT NULL,
     watch_box_present BOOLEAN NOT NULL,
     watch_papers_present BOOLEAN NOT NULL,
     watch_image1 VARCHAR(255) NOT NULL,
     watch_image2 VARCHAR(255) NOT NULL,
-    watch_image3 VARCHAR(255) NOT NULL
+    watch_image3 VARCHAR(255) NOT NULL,
+    stripe_product_id VARCHAR(255) DEFAULT NULL
 );
