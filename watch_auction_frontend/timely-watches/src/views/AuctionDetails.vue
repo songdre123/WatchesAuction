9fad3333ee7c18f3caa4a4c6b2068765f6ef98d3
e-tayfw <ethantay321@gmail.com>
2024-03-21 14:03:34 +0800

    AuctionDetails Bugs

--- watch_auction_frontend/timely-watches/src/views/AuctionDetails.vue
+++ watch_auction_frontend/timely-watches/src/views/AuctionDetails.vue
@@ -3,16 +3,16 @@
         <v-container>
             <v-row>
                 <v-col cols="6" ml="3">
-                    <v-img :src="watch.imageURL"></v-img>
+                    <v-img :src="Watch.imageURL"></v-img>
                 </v-col>
                 <v-col cols="6">
                     <div>
                         <v-sheet border="md" class="pa-6 mx-auto" color="amber-lighten-5" max-width="500" :style="{ fontFamily: 'Riviera Nights', minHeight: '500px' }">
-                            <h2 class="sheet-title">Watch Name: {{ watch.title }}</h2>
+                            <h2 class="sheet-title">Watch Name: {{ Watch.title }}</h2>
                             <v-divider class="mt-3 mb-3"></v-divider>
                             
                             <span class="d-flex justify-space-between">
-                                <h5 class="mb-3">Start Date of Auction: <u>{{ watch.StartDate }}</u></h5>
+                                <h5 class="mb-3">Start Date of Auction: <u>{{ Watch.StartDate }}</u></h5>
 
                                 
                                 <v-dialog max-width="500">
@@ -22,7 +22,7 @@
                                     <template v-slot:default="{ isActive }">
                                         <v-card title="Confirm Bid of Amount">
                                             <v-card-text>
-                                                Please confirm that you are willing and able to make a total bid of $SGD {{ watch.CurrentPrice }}
+                                                Please confirm that you are willing and able to make a total bid of $SGD {{ Watch.CurrentPrice }}
                                             </v-card-text>
 
                                             <v-card-subtitle class="text-white-50" style="white-space: normal;">
@@ -45,13 +45,13 @@
                                     </template>
                                 </v-dialog>
                             </span>
-                            <h5 class="mb-3">End Date of Auction: <u>{{ watch.EndDate }}</u></h5>
+                            <h5 class="mb-3">End Date of Auction: <u>{{ Watch.EndDate }}</u></h5>
 
                             <v-divider class="mt-3 mb-3"></v-divider>
 
-                            <h5 class="mb-3">Current Bid: {{ watch.CurrentPrice }} SGD</h5>
-                            <h5 class="mb-3">Minimum Bid: {{ watch.MinBid }} SGD</h5>
-                            <p class="mb-3">Watch Condition: {{ watch.Condition }}</p>
+                            <h5 class="mb-3">Current Bid: {{ Watch.CurrentPrice }} SGD</h5>
+                            <h5 class="mb-3">Minimum Bid: {{ Watch.MinBid }} SGD</h5>
+                            <p class="mb-3">Watch Condition: {{ Watch.Condition }}</p>
                             <p class="mb-3">Watch Box Present: {{ isWatchBoxPresent }}</p>
                             <p class="mb-3">Watch Papers Present: {{ isWatchPapersPresent }}</p>
                             
@@ -68,7 +68,7 @@
 
 <script>
 import { useWatchStore } from '@/store/watchStore'
-import { ref, computed } from 'vue'
+import { watch, ref, computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { useUserStore } from '@/store/userStore'
 import axios from 'axios'
@@ -80,7 +80,7 @@ export default {
         // const watchStore = useWatchStore()
         // const userStore = useUserStore()
         const isActive = ref(false)
-        const watch =  watchStore.getWatch()
+        const Watch =  ref(watchStore.getWatch())
         const user = userStore.getUser()
         const userId = computed(() => userStore.userID)
 
@@ -88,8 +88,8 @@ export default {
         const id = computed(() => route.params.id)
 
         const isBidEnabled = computed(() => {
-            const watchStartDate = new Date(watch.StartDate)
-            const watchEndDate = new Date(watch.EndDate)
+            const watchStartDate = new Date(Watch.value.StartDate)
+            const watchEndDate = new Date(Watch.value.EndDate)
 
              // 2 hours in milliseconds
             const currentDate = new Date()
@@ -98,26 +98,34 @@ export default {
         })
 
         const isWatchBoxPresent = computed(() => {
-            const watchBoxPresent = watch.WatchBoxPresent
+            const watchBoxPresent = Watch.value.WatchBoxPresent
             return watchBoxPresent ? 'Present' : 'Not Present'
         })
 
         const isWatchPapersPresent = computed(() => {
-            const watchPapersPresent = watch.WatchPapersPresent
+            const watchPapersPresent = Watch.value.WatchPapersPresent
             return watchPapersPresent ? 'Present' : 'Not Present'
+        })
+        // Watch for changes in the current price using a watcher
+        watch(() => watchStore.getCurrentPrice, (newPrice) => {
+            // Update the watch value with the new current price
+            Watch.value.CurrentPrice = newPrice
         })
 
         const createBid = async () => {
-            let price;
-            price = watchStore.getCurrentPrice 
-            
-
+            let price
+            if (Watch.value.CurrentPrice > Watch.value.MinBid) {
+                watchStore.incrementCurrentPrice(500)
+                price = watchStore.getCurrentPrice
+            }
+            else {
+                price = watchStore.getCurrentPrice
+            }
             const bidParams = {
-                "auction_id" : watch.auctionID,
+                "auction_id" : Watch.value.auctionID,
                 "bid_amount" : price,
                 "user_id" : userId.value
             }
-            watchStore.incrementCurrentPrice(500)
             try {
                 await axios.post(`http://127.0.0.1:5006/authbid`, bidParams);
                 isActive.value = false;
@@ -127,11 +135,15 @@ export default {
                 throw error; // Re-throw the error to propagate it further
             }
 
+            if (Watch.value.CurrentPrice == Watch.value.MinBid) {
+                 watchStore.incrementCurrentPrice(500)
+            }
+
         }
 
         
 
-        return { watch, user, userId, id, isBidEnabled, isWatchBoxPresent, isWatchPapersPresent, createBid, isActive}
+        return { Watch, user, userId, id, isBidEnabled, isWatchBoxPresent, isWatchPapersPresent, createBid, isActive}
     }
 }
 ;
