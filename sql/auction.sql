73f9d299a8e597cad464c17397716e92b1c5608c
solai-y <solaiy17112001@gmail.com>
2024-03-28 14:47:21 +0800

    bug fixes
    updates auction winner when rolled back

--- sql/auction.sql
+++ sql/auction.sql
@@ -21,7 +21,7 @@ CREATE TABLE Auction (
     watch_image1 VARCHAR(255) NOT NULL,
     watch_image2 VARCHAR(255) NOT NULL,
     watch_image3 VARCHAR(255) NOT NULL,
-    stripe_product_id VARCHAR(255) DEFAULT NULL
+    stripe_product_id VARCHAR(500) DEFAULT NULL
 );
 
 INSERT INTO Auction (auction_item, start_time, end_time, start_price, manufacture_year, current_price, auction_winner_id, auction_status, watch_ref, watch_condition, watch_brand, watch_box_present, watch_papers_present, watch_image1, watch_image2, watch_image3, stripe_product_id) 
